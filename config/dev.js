b8cd83d3d07f8307136fd6f9307828b84c989023
Guy Livni <guylivni@gmail.com>
2017-10-07 17:41:49 +0300

    finished auth flow

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID: '1096140790643-ou2c2mnoslohfe4tqaduvhlbr8diooqu.apps.googleusercontent.com',
+  googleClientSecret: 'rzfQFZ2N4deOq2DS20KIxWnQ',
+  mongoURI: 'mongodb://guylivni:nanana232@ds149954.mlab.com:49954/gl-feedback',
+  cookieKey: 'lkmzxcaksdnvlkjasdnqlsaldlknvlnasdnlaksndlkma'
+};
