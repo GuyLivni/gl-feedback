d334bbaf6dcc55a33980749d8bde90845b4cc6f6
Guy Livni <guylivni@gmail.com>
2017-12-11 14:24:08 +0200

    add flowconfig

--- client/src/utils/validateEmails.js
+++ client/src/utils/validateEmails.js
@@ -1,7 +1,7 @@
 /* @flow */
 const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/; // eslint-disable-line
 
-export default (emails: String) => {
+export default (emails: string) => {
   const invalidEmails = emails
     .split(',')
     .map(email => email.trim())
