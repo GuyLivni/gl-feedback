abb05c0feb6324ba3e392c110b3fc3b02ed3e628
Guy Livni <guylivni@gmail.com>
2018-01-09 09:37:09 +0200

    add new template, update validator

--- middlewares/validator.js
+++ middlewares/validator.js
@@ -2,23 +2,23 @@ const Joi = require("joi");
 
 module.exports = (schema = {}) => (
   (req, res, next) => {
-    const errors = [];
+    let errors = [];
     Object.keys(schema).forEach((type) => {
       const validated = Joi.validate(req[type], schema[type], { abortEarly: false });
       if (validated.error) {
-        validated.error.details.forEach((error) => {
-          errors.push({
-            status: 400,
-            title: "Bad Parameter",
-            details: error.message,
-            source: req.originalUrl
-          });
-        });
+        errors = validated.error.details.map((error) => ({
+          status: 400,
+          title: "Bad Parameter",
+          details: error.message,
+          source: req.originalUrl
+        }));
       }
     });
+
     if (errors.length) {
       return res.status(400).send({ errors });
     }
+
     return next();
   }
 );
--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -4,16 +4,30 @@ module.exports = survey => {
   return `
     <html>
       <body>
-        <div style="text-align: center;">
-            <h3>I'd like your input!</h3>
-            <p>Please answer the following question:</p>
+        <div style="width: 90%; margin: 50px auto;">
+        <div style="margin: 50px 0;">
+            <span style="font-size: 20px;">
+                <strong>Hi, I'd like your input</strong>
+            </span>
+        </div>
+        <div style="margin: 20px auto;">
+            Please answer the following question regarding, ${survey.title}:
+        </div>
+        <div style="background: #D8D8D8; border: 1px solid #A9A9A9; border-radius: 10px; padding: 5px 10px; margin: 20px auto;">
             <p>${survey.body}</p>
-            <span>
+        </div>
+        <p style="text-align: center; margin-top: 30px">
+            <span style="margin: 10px;">
               <a href="${keys.redirectDomain}/api/surveys/${survey.id}/yes">Yes</a>
             </span>
-            <span>
+            <span style="margin: 10px;">
               <a href="${keys.redirectDomain}/api/surveys/${survey.id}/no">No</a>
             </span>
+        </p>
+        <p>
+           Thank you,<br>
+           The Feedback Team
+        </p>
         </div>
       </body>
     </html>
